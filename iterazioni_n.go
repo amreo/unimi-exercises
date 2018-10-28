49a4829804ddba5a6d1d498cff30580a6417543f
amreo <amreo@live.it>
2018-10-28 20:57:17 +0100

    Fixed errore di comprensione

--- iterazioni_n.go
+++ iterazioni_n.go
@@ -10,10 +10,13 @@ func main() {
 	fmt.Scan(&n)
 
 	//Primi N quadrati
-	fmt.Println("Primi N quadrati")
+	fmt.Print("Somma dei primi N quadrati: ")
+	somma := 0
 	for i := 1; i <= n; i++ {
-		fmt.Println("#", i, " ", i*i)
+		//fmt.Println("#", i, " ", i*i)
+		somma += i * i
 	}
+	fmt.Println(somma)
 
 	//Conto alla rovescia fino a 1
 	fmt.Println("Conto alla rovescia da n a 1")
