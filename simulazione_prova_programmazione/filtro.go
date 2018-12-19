ff2c642d97263bc7a2c602d36a095afedd785b3d
amreo <amreo@live.it>
2018-12-19 08:48:17 +0100

    Added filtro.go

---
+++ simulazione_prova_programmazione/filtro.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+)
+
+//MAPPA costante (più o meno) che mappa i voti alle loro rispettive fascia
+var FASCIA_DI_VALORI map[int]rune = map[int]rune{
+	18: 'D', 19: 'D', 20: 'D', 21: 'D',
+	22: 'C', 23: 'C', 24: 'C',
+	25: 'B', 26: 'B', 27: 'B',
+	28: 'A', 29: 'A', 30: 'A',
+}
+
+//Funzione main
+func main() {
+	frequenzaFascie := make(map[rune]int) //mappa che tiene traccia delle fascie
+	numeroValori := 0                     //Numero di valori immessi finora
+	input := -1                           //Valore immesso dall'input
+
+	//Ciclo di inserimento degli input finché diverso da 0
+	for input != 0 {
+		fmt.Scan(&input)
+		//Se l'input è nella fascia dei voti validi
+		if input >= 18 && input <= 30 {
+			//Lo aggiunge alla rispettiva fascia
+			frequenzaFascie[FASCIA_DI_VALORI[input]]++
+			numeroValori++
+		}
+	}
+
+	//Stampa le info sulle frequenze
+	fmt.Printf("A : %d %%\n", frequenzaFascie['A']*100/numeroValori)
+	fmt.Printf("B : %d %%\n", frequenzaFascie['B']*100/numeroValori)
+	fmt.Printf("C : %d %%\n", frequenzaFascie['C']*100/numeroValori)
+	fmt.Printf("D : %d %%\n", frequenzaFascie['D']*100/numeroValori)
+}
